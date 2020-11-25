b1a3a4ab8fced75d5c7a7f85490a95e303d83104
Teng Wei Song <weisong.teng.work@gmail.com>
2020-11-25 16:29:35 +0800

    add bulma

---
+++ src/components/Page.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="container">
+    <h1 class="title">{{ title }}</h1>
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    title: String
+  }
+};
+</script>
---
+++ src/pages/Home.vue
@@ -0,0 +1,12 @@
+<template>
+  <page title="Home">
+    <p>hi this is home</p>
+  </page>
+</template>
+
+<script>
+import Page from "../components/Page";
+export default {
+  components: { Page }
+};
+</script>
