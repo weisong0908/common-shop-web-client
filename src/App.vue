7ec7690c28cb890cb9a6cd32b079574ceae1feb5
Teng Wei Song <weisong.teng.work@gmail.com>
2020-11-26 23:43:01 +0800

    add footer

--- src/App.vue
+++ src/App.vue
@@ -2,16 +2,19 @@
   <div id="app">
     <navbar></navbar>
     <router-view></router-view>
+    <app-footer></app-footer>
   </div>
 </template>
 
 <script>
 import Navbar from "./components/Navbar";
+import AppFooter from "./components/Footer";
 
 export default {
   name: "App",
   components: {
-    Navbar
+    Navbar,
+    AppFooter
   }
 };
 </script>
---
+++ src/components/Footer.vue
@@ -0,0 +1,10 @@
+<template>
+  <footer class="footer">
+    <div class="content has-text-centered">
+      <p>
+        <strong>Common Shop</strong> by
+        <a href="https://github.com/weisong0908">Teng Wei Song</a>.
+      </p>
+    </div>
+  </footer>
+</template>
