a4b8b7d90d0337be99ef0974c0a53ccdf668502f
Teng Wei Song <weisong.teng.work@gmail.com>
2020-11-28 11:20:34 +0800

    add modal card

---
+++ src/components/Modal.vue
@@ -0,0 +1,26 @@
+<template>
+  <div :class="isActive ? 'modal is-active' : 'modal'">
+    <div class="modal-background" @click="toggle"></div>
+    <div class="modal-content">
+      <div class="box">
+        <slot></slot>
+      </div>
+    </div>
+    <button
+      class="modal-close is-large"
+      aria-label="close"
+      @click="toggle"
+    ></button>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["isActive"],
+  methods: {
+    toggle() {
+      this.$emit("toggle");
+    }
+  }
+};
+</script>
--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,12 +1,28 @@
 <template>
   <page title="Home">
     <p>hi this is home</p>
+    <modal :isActive="showModal" @toggle="showModal = !showModal">
+      <p>This is a modal</p>
+    </modal>
+    <button @click="doshowmodal"></button>
   </page>
 </template>
 
 <script>
 import Page from "../components/Page";
+import Modal from "../components/Modal";
+
 export default {
-  components: { Page }
+  components: { Page, Modal },
+  data() {
+    return {
+      showModal: false
+    };
+  },
+  methods: {
+    doshowmodal() {
+      this.showModal = true;
+    }
+  }
 };
 </script>
