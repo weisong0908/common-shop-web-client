bbdc9f703f239d50974e6a5d51550b09ed21bc1d
weisong0908 <weisong.teng.work@gmail.com>
2020-11-29 17:08:42 +0800

    update footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,10 +1,42 @@
 <template>
   <footer class="footer">
-    <div class="content has-text-centered">
-      <p>
-        <strong>Common Shop</strong> by
-        <a href="https://github.com/weisong0908">Teng Wei Song</a>.
-      </p>
+    <div class="content">
+      <p class="title is-4">Common Shop</p>
+      <div class="columns">
+        <div class="column">
+          <p class="title is-6">About This Project</p>
+          <img
+            src="../assets/logo.png"
+            style="height: 2rem"
+            alt="Project logo"
+          />
+          <p>
+            The <em>Common Shop</em> is a project by
+            <a href="https://github.com/weisong0908">Teng Wei Song</a> to
+            demonstrate building an imaginary e-commerce website with Vue and
+            microservices (.NET).
+          </p>
+        </div>
+        <div class="column">
+          <p class="title is-6">About This Web Client</p>
+          <p>
+            The web client is a Single Page Application (SPA) made with
+            <a href="https://vuejs.org">Vue</a> and
+            <a href="https://bulma.io">Bulma</a>
+            .
+          </p>
+          <a href="https://bulma.io">
+            <img
+              src="https://bulma.io/images/made-with-bulma.png"
+              alt="Made with Bulma"
+              width="128"
+              height="24"
+              title="Bulma"
+            />
+          </a>
+        </div>
+        <div class="column"></div>
+      </div>
     </div>
   </footer>
 </template>
