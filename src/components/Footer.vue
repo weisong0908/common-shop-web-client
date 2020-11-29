50fb5dd2ed94f4ae7334319189a2f5afa1aab6e5
Teng Wei Song <weisong.teng.work@gmail.com>
2020-11-30 00:17:29 +0800

    update footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -5,37 +5,58 @@
       <div class="columns">
         <div class="column">
           <p class="title is-6">About This Project</p>
-          <img
-            src="../assets/logo.png"
-            style="height: 2rem"
-            alt="Project logo"
-          />
           <p>
-            The <em>Common Shop</em> is a project by
-            <a href="https://github.com/weisong0908">Teng Wei Song</a> to
+            Currently under development, the <em>Common Shop</em> is a project
+            by <a href="https://github.com/weisong0908">Teng Wei Song</a> to
             demonstrate building an imaginary e-commerce website with Vue and
             microservices (.NET).
           </p>
+          <p>
+            Click
+            <a href="https://github.com/weisong0908/common-shop-web-client"
+              >here</a
+            >
+            to check out the project repository on GitHub.
+          </p>
+          <div class="control">
+            <div class="tags has-addons">
+              <span class="tag is-dark">Version</span>
+              <span class="tag is-info">Alpha</span>
+            </div>
+          </div>
         </div>
         <div class="column">
           <p class="title is-6">About This Web Client</p>
           <p>
-            The web client is a Single Page Application (SPA) made with
-            <a href="https://vuejs.org">Vue</a> and
-            <a href="https://bulma.io">Bulma</a>
-            .
+            The web client is a single page application (SPA) made with
+            <strong>Vue</strong> Javascript framework and
+            <strong>Bulma</strong> CSS framework.
           </p>
-          <a href="https://bulma.io">
-            <img
-              src="https://bulma.io/images/made-with-bulma.png"
-              alt="Made with Bulma"
-              width="128"
-              height="24"
-              title="Bulma"
-            />
-          </a>
+          <div>
+            <a href="https://vuejs.org/" class="p-0">
+              <img
+                src="https://raw.githubusercontent.com/vuejs/art/master/vue-badge.png"
+                style="height:2.5rem"
+                alt="Made with Vue"
+                title="Vue.Js"
+              />
+            </a>
+          </div>
+          <div>
+            <a href="https://bulma.io">
+              <img
+                src="https://bulma.io/images/made-with-bulma.png"
+                alt="Made with Bulma"
+                width="128"
+                height="24"
+                title="Bulma"
+              />
+            </a>
+          </div>
         </div>
-        <div class="column"></div>
+        <div class="column">
+          <p class="title is-6">The Author</p>
+        </div>
       </div>
     </div>
   </footer>
